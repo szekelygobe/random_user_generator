e1a2fbbdf2d8aacba3b66eb8ee0a2e57ae9bfa88
Szekely Csaba <csaba@szekely.ro>
2021-03-28 13:53:02 +0300

    Basic setup done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>'
+    data(){
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
 })
 
 app.mount('#app')
